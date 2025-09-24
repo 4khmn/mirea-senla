76bcd294444ee174f047a196f269e258e0d88e93
4khmn <denis-akhmin@yandex.ru>
2025-09-24 21:32:38 +0300

    add toLowerCase in psvm

--- src/Task1/Runner.java
+++ src/Task1/Runner.java
@@ -9,7 +9,7 @@ public class Runner {
     public static void main(String[] args) throws InterruptedException {
         List<String> arr = List.of("пакет", "инкапсуляция", "программирование", "монитор", "анализ", "сервер");
         Random random = new Random();
-        String word = (String)arr.get(random.nextInt(5));
+        String word = (String)arr.get(random.nextInt(5)).toLowerCase();
         Game game = new Game(word);
         game.start();
     }
